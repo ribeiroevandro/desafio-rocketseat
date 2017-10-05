cbdab82f5f2f64e81d66ab02da0703fbdc3903dd
Evandro Ribeiro <ribeiroevandro@live.com>
2017-10-05 13:34:39 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Quero uma Caneca da Rockeat Seat</h1>
+        <h1>Quero uma Caneca da Rocket Seat</h1>
       </div>
     );
   }
